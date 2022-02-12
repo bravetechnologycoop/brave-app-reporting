125a3fb84377f00ba3e499a122c3a85e00597e18
trisapeace <theresa@brave.coop>
2022-02-11 17:23:31 -0800

    Get Apple Connect first time download stats and store in DB

---
+++ apple.js
@@ -0,0 +1,49 @@
+// Code inspired by https://developer.apple.com/forums/thread/667147
+
+// Third-party dependencies
+const axios = require('axios')
+const readline = require('readline')
+
+const url = 'https://appstoreconnect.apple.com/analytics/api/v1/data/time-series'
+
+async function getAppleFirstTimeDownloads(appId) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+  console.log('Authorize this app by visiting this url and logging in using your Apple ID: https://appstoreconnect.apple.com/login')
+  const cookies = await new Promise(resolve => {
+    rl.question('Copy all the cookies sent to "/providerNews" from the Developer Console Network tab and enter them here : ', resolve)
+  })
+  rl.close()
+
+  const response = await axios({
+    method: 'post',
+    url,
+    data: {
+      adamId: [appId],
+      measures: ['units'],
+      frequency: 'day',
+      startTime: '2018-09-01T00:00:00Z', // Month where we first have Brave App data
+      endTime: `${new Date().toISOString().substring(0, 10)}T00:00:00Z`,
+      group: {
+        metric: 'units',
+        dimension: 'storefront',
+        rank: 'DESCENDING',
+        limit: 10,
+      },
+    },
+    headers: {
+      Host: 'appstoreconnect.apple.com',
+      'X-Requested-By': 'dev.apple.com',
+      Cookie: cookies,
+      'Content-Type': 'application/json',
+    },
+  })
+
+  return response.data.results
+}
+
+module.exports = {
+  getAppleFirstTimeDownloads,
+}
--- setup_postgresql.sh
+++ setup_postgresql.sh
@@ -2,3 +2,4 @@ sudo PGPASSWORD=$PG_PASSWORD psql -U $PG_USER -h $PG_HOST -p $PG_PORT -d $PG_DAT
 sudo PGPASSWORD=$PG_PASSWORD psql -U $PG_USER -h $PG_HOST -p $PG_PORT -d $PG_DATABASE --set=sslmode=require -f ./db/001-setup.sql
 sudo PGPASSWORD=$PG_PASSWORD psql -U $PG_USER -h $PG_HOST -p $PG_PORT -d $PG_DATABASE --set=sslmode=require -f ./db/002-create-calls-table.sql
 sudo PGPASSWORD=$PG_PASSWORD psql -U $PG_USER -h $PG_HOST -p $PG_PORT -d $PG_DATABASE --set=sslmode=require -f ./db/003-create-supportlogs-table.sql
+sudo PGPASSWORD=$PG_PASSWORD psql -U $PG_USER -h $PG_HOST -p $PG_PORT -d $PG_DATABASE --set=sslmode=require -f ./db/004-create-appledownloads-table.sql
